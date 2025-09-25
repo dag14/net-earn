5879d3df9f0c02853235f33f7af3d445f614d7e2
Dagim Asrat <dagimasrat3@gmail.com>
2025-09-25 11:52:58 +0300

    setup go_router

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,27 @@
+import 'package:go_router/go_router.dart';
+// import '../../features/income_calculator/presentation/calculator_screen.dart';
+// import '../../features/income_calculator/presentation/history_screen.dart';
+// import '../../features/income_calculator/presentation/insights_screen.dart';
+// import '../../features/settings/presentation/settings_screen.dart';
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/calculator',
+  routes: [
+    // GoRoute(
+    //   path: '/calculator',
+    //   builder: (context, state) => const CalculatorScreen(),
+    // ),
+    // GoRoute(
+    //   path: '/history',
+    //   builder: (context, state) => const HistoryScreen(),
+    // ),
+    // GoRoute(
+    //   path: '/insights',
+    //   builder: (context, state) => const InsightsScreen(), // Coming Soon
+    // ),
+    // GoRoute(
+    //   path: '/settings',
+    //   builder: (context, state) => const SettingsScreen(),
+    // ),
+  ],
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:netearn/core/routes/app_router.dart';
 import 'core/theme/app_theme.dart';
 import 'core/providers/app_providers.dart';
 
@@ -14,12 +15,12 @@ class NetEarnApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final themeMode = ref.watch(themeProvider);
 
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'NetEarn',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: themeMode, // dynamic toggle
-      home: const Placeholder(), // will replace with GoRouter soon
+      routerConfig: appRouter,
     );
   }
 }
