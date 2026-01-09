6e8b841ecdf44d39885ea12622df6596c23848cc
Dagim Asrat <dagimasrat3@gmail.com>
2026-01-09 11:49:51 +0300

    ui: add simple and advanced subtabs on the calc screen

--- lib/core/presentation/screens/calculator_screen.dart
+++ lib/core/presentation/screens/calculator_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:netearn/core/widgets/calculator_tapbar.dart';
 import '../../widgets/default_appbar.dart';
 
 class CalculatorScreen extends StatelessWidget {
   const CalculatorScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const DefaultAppBar(),
-      body: const Center(child: Text('Calculator Screen Content')),
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 2,
+      child: Scaffold(
+        appBar: const DefaultAppBar(
+          title: 'Salary Calculator',
+          icon: Icons.calculate,
+          bottom: CalculatorTabBar(
+            icons: [Icon(Icons.auto_awesome), Icon(Icons.build)],
+            texts: ['Simple - Gross', 'Advanced - + Allowances'],
+          ),
+        ),
+        body: const Center(child: Text('Calculator Screen Content')),
+      ),
     );
   }
 }
